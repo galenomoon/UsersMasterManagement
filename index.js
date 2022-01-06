632185a921ec10325d73362013bc469105f9a975
galeno <gui.galenocover@gmail.com>
2022-01-06 17:17:28 -0300

    UserManagement Finished

---
+++ index.js
@@ -0,0 +1,19 @@
+//=============================== NOTES ===============================
+/*
+JSON = JAVASCRIPT OBJECTIVE NOTATION  = {key:value}//
+// OBJETO | É uma representação da Classe, tendo acesso a atributos e métodos definidos na classe a qual este representa
+SPA | Single Page Application | (like Gmail)
+
+=== FUNTION STRUCTURE === 
+         P //What We'll receive?
+Function(Parameter){
+}
+
+IF TERNÁRIO| condição ? true : false
+
+FILEREADER | Útil para ler e manioular arquivos e pastas (API NATIVA DO JS)
+=== * S T A R T * =========== SECTION 3: USER MANEGEMENT ======== * S T A R T * === */
+
+
+// go to the document and query all ("of this condition of this type") and select them
+let userController = new UserController("form-user-create", "form-user-update", "table-users")
